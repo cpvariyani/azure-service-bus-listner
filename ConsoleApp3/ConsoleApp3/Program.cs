0bb35f8edd54dc2479e2f288994e43876b302614
V-Chandra Variyani <Chandra.Variyani@blackbaud.me>
2019-04-03 12:10:02 +0530

    data

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Azure.ServiceBus;
+using Newtonsoft.Json;
+using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static ISubscriptionClient subscriptionClient;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Listner started.");
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync()
+        {
+            string ServiceBusConnectionString = "your-service-bus-listen-key";
+            string TopicName = "your-topic-name";
+            string SubscriptionName = "your-subscription-name";
+
+            subscriptionClient = new SubscriptionClient(ServiceBusConnectionString, TopicName, SubscriptionName);
+
+            // Register subscription message handler and receive messages in a loop.
+            RegisterOnMessageHandlerAndReceiveMessages();
+
+            Console.ReadKey();
+
+            await subscriptionClient.CloseAsync();
+        }
+
+        static void RegisterOnMessageHandlerAndReceiveMessages()
+        {
+            var messageHandlerOptions = new MessageHandlerOptions(ExceptionReceivedHandler);
+
+            // Register the function that processes messages.
+            subscriptionClient.RegisterMessageHandler(ProcessMessagesAsync, messageHandlerOptions);
+        }
+
+        static async Task ProcessMessagesAsync(Message message, CancellationToken token)
+        {
+            var messageBody = Encoding.UTF8.GetString(message.Body);
+
+            var serviceBusMessage = JsonConvert.DeserializeObject<ServiceBusMessage>(messageBody);
+
+            Console.WriteLine($"Received message: UserInfo:{Encoding.UTF8.GetString(message.Body)}");
+
+            await subscriptionClient.CompleteAsync(message.SystemProperties.LockToken);
+        }
+
+        static Task ExceptionReceivedHandler(ExceptionReceivedEventArgs exceptionReceivedEventArgs)
+        {
+            var exception = exceptionReceivedEventArgs.Exception;
+
+            return Task.CompletedTask;
+        }
+
+        public class ServiceBusMessage
+        {
+            public string Id { get; set; }
+            public string Type { get; set; }
+            public string Content { get; set; }
+        }
+    }
+}
